63cb64ec2af92761427740badfafb82a928dd673
Grigory Makarov <makkgregory@gmail.com>
2019-11-10 04:24:28 +0300

    refactor(uikit): refactor `SearchBox` class and adjust styles and animations

--- src/lib/uikit/search-box/search-box.ts
+++ src/lib/uikit/search-box/search-box.ts
@@ -15,23 +15,12 @@
  */
 import {View} from '../view';
 import {Icon, TagName} from "@telegram/uikit";
-import {Subject} from "rxjs";
-import {distinctUntilChanged} from "rxjs/operators";
+import {fromEvent, Observable} from "rxjs";
+import {distinctUntilChanged, map} from "rxjs/operators";
 
 const style = require('./search-box.scss');
 
 export class SearchBox extends View {
-
-    private readonly queryStream = new Subject<string>();
-
-    public readonly query$ = this.queryStream.pipe(
-        distinctUntilChanged()
-    );
-
-    public get query(): string {
-        return this.inputView.element.value;
-    }
-
     private readonly iconView: Icon = (() => {
         const view = new Icon(require('assets/icons/search_svg.svg'));
         view.addClassName(style.icon);
@@ -46,6 +35,19 @@ export class SearchBox extends View {
         this.addSubview(view);
         return view;
     })();
+
+    public readonly query$: Observable<string> = fromEvent(this.inputView.element, 'input').pipe(
+        map(() => this.query),
+        distinctUntilChanged()
+    );
+
+    public get query(): string {
+        return this.inputView.element.value;
+    }
+
+    public set query(value: string) {
+        this.inputView.element.value = value;
+    }
 
     private readonly labelView: View = (() => {
         const view = new View(TagName.span);
@@ -66,22 +68,23 @@ export class SearchBox extends View {
         super();
         this.label = "Search";
         this.addClassName(style.searchField);
-        this.setupQueryListener();
+
+        fromEvent(this.element, 'click').subscribe(() => this.inputView.element.focus());
+
+        this.setupHidingLabelFeature();
     }
 
-    private setupQueryListener() {
-        this.inputView.element.oninput = () => this.queryStream.next(this.query);
-
-        let labelHidden = false;
+    private setupHidingLabelFeature() {
+        let hasHiddenClass = false;
 
         this.query$.subscribe(query => {
-            if (query && !labelHidden) {
+            if (query && !hasHiddenClass) {
                 this.labelView.addClassName(style.hidden);
-                labelHidden = true;
-            } else if (!query && labelHidden) {
+                hasHiddenClass = true;
+            } else if (!query && hasHiddenClass) {
                 this.labelView.removeClassName(style.hidden);
-                labelHidden = false;
+                hasHiddenClass = false;
             }
-        })
+        });
     }
 }
