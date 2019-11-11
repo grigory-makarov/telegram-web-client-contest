b8b8439e00bc177f5f33323af3a665c44077cb48
Grigory Makarov <makkgregory@gmail.com>
2019-11-11 23:22:13 +0300

    feat(uikit): add lifecycle events as observables to `ViewController`

--- src/lib/uikit/view-controller.ts
+++ src/lib/uikit/view-controller.ts
@@ -16,10 +16,39 @@
 
 import {View} from "./view";
 import {environment} from "../../environments/environment";
+import {Subject} from "rxjs";
+import {filter, map} from "rxjs/operators";
 
+enum LifecycleEvent {
+    viewWillAppear,
+    viewDidAppear,
+    viewWillDisappear,
+    viewDidDisappear
+}
 
 export abstract class ViewController<TView extends View = View> {
     private _view: TView | null = null;
+    private readonly lifecycleEventSubject = new Subject<LifecycleEvent>();
+
+    public readonly viewWillAppear$ = this.lifecycleEventSubject.pipe(
+        filter(event => event === LifecycleEvent.viewWillAppear),
+        map(() => undefined as void)
+    );
+
+    public readonly viewDidAppear$ = this.lifecycleEventSubject.pipe(
+        filter(event => event === LifecycleEvent.viewDidAppear),
+        map(() => undefined as void)
+    );
+
+    public readonly viewWillDisappear$ = this.lifecycleEventSubject.pipe(
+        filter(event => event === LifecycleEvent.viewWillDisappear),
+        map(() => undefined as void)
+    );
+
+    public readonly viewDidDisappear$ = this.lifecycleEventSubject.pipe(
+        filter(event => event === LifecycleEvent.viewDidDisappear),
+        map(() => undefined as void)
+    );
 
     public get view(): TView {
         if (!this._view) {
@@ -36,14 +65,18 @@ export abstract class ViewController<TView extends View = View> {
     public abstract createView(): TView;
 
     public viewWillAppear() {
+        this.lifecycleEventSubject.next(LifecycleEvent.viewWillAppear);
     }
 
     public viewDidAppear() {
+        this.lifecycleEventSubject.next(LifecycleEvent.viewDidAppear);
     }
 
     public viewWillDisappear() {
+        this.lifecycleEventSubject.next(LifecycleEvent.viewWillDisappear);
     }
 
     public viewDidDisappear() {
+        this.lifecycleEventSubject.next(LifecycleEvent.viewDidDisappear);
     }
 }
